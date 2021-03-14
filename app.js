7a9675721cb10cdc17aa619bb229476f6d757507
andreixxi <45465662+andreixxi@users.noreply.github.com>
2021-03-14 19:38:20 +0200

    functional menu
    !!! redo the css

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-$(function() {
+$(function () {
     // DO NOT DELETE
     $('#fullpage').fullpage({
-        'verticalCentered': false,
-        'css3': true,
-        'navigation': true,
-        'navigationPosition': 'right',
+        menu: '#menu',
+        anchors: ['main', '3d-view', 'images', 'videos'],
+        sectionsColor: ['', '#7BAABE', '#7BAABE', '#7BAABE'], //Define the CSS background-color property for each section
+        css3: true,
+        controlArrows: true, //Determines whether to use control arrows for the slides to move right or left.
+        // fixedElements: '#sidebar',
+
     });
 
+    //menu
+    $('.navigation .toggle-wrapper .show').on('click', function () {
+        $('.navigation').addClass('open');
+    });
+
+    $('.navigation .toggle-wrapper .hide').on('click', function () {
+        $('.navigation').removeClass('open');
+        // $('.navigation.menu').hide();
+    });
+
+    $('.navigation .menu li').on('click', function() {
+        $('.navigation').removeClass('open');
+    });
+    
+    //////////////////////////////////////////////////////////////
     // for image gallery
     lc_lightbox('.mybox', {
         wrap_class: 'lcl_face_oc',
-        gallery: true, 
+        gallery: true,
         skin: 'dark',
         counter: true,
         fullscreen: true,
@@ -22,10 +40,35 @@ $(function() {
         border_col: '#ddd',
         radius: 5
     });
-
+    //////////////////////////////////////////////////////////////
     // video gallery
-    new Splide( '.splide', {
+
+    new Splide('.splide', {
         perPage: 3,
-        rewind : true,
-    } ).mount(window.splide.Extensions);
+        rewind: true,
+    }).mount(window.splide.Extensions);
+    //////////////////////////////////////////////////////////////
+    // MENU
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#dismiss, .overlay').on('click', function () {
+        // hide sidebar
+        $('#sidebar').removeClass('active');
+        // hide overlay
+        $('.overlay').removeClass('active');
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        // open sidebar
+        $('#sidebar').addClass('active');
+        // fade in the overlay
+        $('.overlay').addClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+
+
 });
